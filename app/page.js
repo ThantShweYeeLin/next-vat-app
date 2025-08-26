ab3d5fa717a4f881bfa44b3696c0cf1df4b32c20
ThantShweYeeLin <u6632067@au.edu>
2025-08-27 01:54:40 +0700

    Fix Errors

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Next.js App</h1>
+      <nav>
+        <ul>
+          <li><a href="/vat">VAT Calculator</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
